7a425a172c7c4aae1c8c57076a806fe783ceb45c
Petrina Lindqvist <petrina.lindqvist@centria.fi>
2020-10-12 13:05:43 +0300

    production build

---
+++ build/precache-manifest.7adca689c83e91a0f1b8c1521c5fd831.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "df5f913c6a2d94acf1f89323d7cf3c39",
+    "url": "/index.html"
+  },
+  {
+    "revision": "98b6557764f27de9c725",
+    "url": "/static/css/main.ab37bccc.chunk.css"
+  },
+  {
+    "revision": "10822974f48efc347430",
+    "url": "/static/js/2.badec770.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.badec770.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "98b6557764f27de9c725",
+    "url": "/static/js/main.14618b51.chunk.js"
+  },
+  {
+    "revision": "f4d2ac87a76b6331fd91",
+    "url": "/static/js/runtime-main.4234e0d5.js"
+  }
+]);
