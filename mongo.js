5e0bc2ea1a36ea81444ec59273bed7ab422bfd49
Petrina Lindqvist <petrina.lindqvist@centria.fi>
2020-10-14 14:30:01 +0300

    Ex. 3.11 and 3.12 done

---
+++ mongo.js
@@ -0,0 +1,44 @@
+const mongoose = require('mongoose')
+
+const password = process.argv[2]
+const name = process.argv[3]
+const number = process.argv[4]
+
+const url =
+  `mongodb+srv://fullstack:${password}@cluster0.9baaj.mongodb.net/persons-app?retryWrites=true&w=majority`
+  
+
+mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true})  
+
+if (process.argv.length<3) {
+  console.log('give password as argument')
+  process.exit(1)
+}
+
+const personSchema = new mongoose.Schema({
+    name: String,
+    number: String
+})
+const Person = mongoose.model('Person', personSchema)
+
+if (name === undefined || number === undefined) {
+    Person
+    .find({})
+    .then(person => {
+        console.log('Phonebook:')
+        person.map(person => console.log(person.name, person.number))
+        mongoose.connection.close()
+})
+}
+else
+{
+    const person = new Person({
+    name: name,
+    number: number,
+})
+
+person.save().then(response => {
+  console.log(`${name} ${number} added to phonebook`)
+  mongoose.connection.close()
+})
+}
